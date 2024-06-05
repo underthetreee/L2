74ed3fffa5010d80f6063ba9824d434401a47ff2
underthetreee <153267269+underthetreee@users.noreply.github.com>
2024-06-05 14:12:51 +0000

    Implement builder pattern

--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,77 @@
 package pattern
 
 /*
-	Реализовать паттерн «строитель».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Builder_pattern
+   Паттерн "строитель" используется для создания сложных объектов пошагово.
+   Позволяет создавать объекты различной структуры и конфигурации, изолируя
+   процесс создания объекта от представления.
+
+   Плюсы:
+   1) Позволяет разделить процесс конструирования и представления сложного объекта
+   2) Позволяет изменять внутренню структуру создаваемого объекта, не меняя представление
+   3) Повышение читаемости кода, путем разделения создания объектов на отдельные шаги
+
+   Минусы:
+   1) В случае создания простых объектов может привести к избыточности кода
+   2) Переизбыток структур, так как для каждого типа объекта необходим свой строитель
+
+   Примеры использования:
+   1) Построение HTTP-запроса
+   2) Создание конфигурации для приложения
+
 */
+
+type Builder interface {
+	SetName(name string)
+	SetComponentA(name string)
+	SetComponentB(name string)
+	Build() Product
+}
+
+type Product struct {
+	name       string
+	componentA string
+	componentB string
+}
+
+type ConcreteBuilder struct {
+	p Product
+}
+
+func (b *ConcreteBuilder) SetName(name string) {
+	b.p.name = name
+}
+
+func (b *ConcreteBuilder) SetComponentA(component string) {
+	b.p.componentA = component
+}
+
+func (b *ConcreteBuilder) SetComponentB(component string) {
+	b.p.componentB = component
+}
+
+func (b *ConcreteBuilder) Build() Product {
+	return b.p
+}
+
+type Director struct {
+	b Builder
+}
+
+func NewDirector(b Builder) *Director {
+	return &Director{
+		b: b,
+	}
+}
+
+func (d *Director) Construct() Product {
+	d.b.SetName("name")
+	d.b.SetComponentA("componentA")
+	d.b.SetComponentB("componentB")
+	return d.b.Build()
+}
+
+func main() {
+	cb := &ConcreteBuilder{}
+	d := NewDirector(cb)
+	p := d.Construct()
+}
