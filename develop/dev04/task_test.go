37eaf73e5e4d0517fead138db927b10df22bcbb1
underthetreee <153267269+underthetreee@users.noreply.github.com>
2024-06-10 22:53:15 +0000

    Implement anagram

--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindAnagrams(t *testing.T) {
+	words := []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик"}
+	expectedResult := map[string][]string{
+		"акптя":  {"пятак", "пятка", "тяпка"},
+		"иклост": {"листок", "слиток", "столик"},
+	}
+
+	result := findAnagrams(words)
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("findAnagrams() = %v, want %v", result, expectedResult)
+	}
+}
